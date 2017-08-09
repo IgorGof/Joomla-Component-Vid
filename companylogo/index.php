821117f25ffb55e0a2c5e5867e0cd8877e1acb78
IgorGof <igorgof@yandex.ru>
2017-08-09 19:40:05 +0500

    Начало верстки шаблона

--- companylogo/index.php
+++ companylogo/index.php
@@ -1,4 +1,21 @@
 <?php
-    echo JText::_("TPL_COMPANYLOGO_CUSTOM")
+    defined("_JEXEC") or die();
+    $doc = new JFactory::getDocument();
+    doc->addStyleSheet(JUri::base(true)."/templates/".$doc->template."/style.css");
 ?>
-<h1>Hello</h1>
+<!DOCTYPE html>
+<html>
+    <head>
+        <jdoc::include type="head">
+    </head>
+    <body>
+        <?php print_r($doc)?>
+        <div class="wrap">
+            <div class="head">
+                <a href="/">
+                    <img closs=logo src="<?php echo JUri::base(true)?>">
+                </a>
+            </div>
+        </div>
+    </body>
+</html>
